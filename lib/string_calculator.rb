68ca7389e0dc24705f494fac6b671949a2f74d08
Nirmal Yadav <nirmal.yadav00@gmail.com>
2024-05-01 00:10:35 +0530

    Add changes to return the sum of two numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,10 @@ class StringCalculator
   def add(numbers)
     0 if numbers.empty?
 
-    numbers.to_i
+    if numbers.include?(',')
+      numbers.split(',').map(&:to_i).sum
+    else
+      numbers.to_i
+    end
   end
 end
