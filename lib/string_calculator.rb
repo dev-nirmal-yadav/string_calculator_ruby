5b137a5e01925116695a27c744175179c8fe5302
Nirmal Yadav <nirmal.yadav00@gmail.com>
2024-05-01 00:19:33 +0530

    Refactor Changes to handle any numbers in input using inject.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,11 +1,5 @@
 class StringCalculator
   def add(numbers)
-    0 if numbers.empty?
-
-    if numbers.include?(',')
-      numbers.split(',').map(&:to_i).sum
-    else
-      numbers.to_i
-    end
+    numbers.split(',').inject(0) { |sum, num| sum + num.to_i }
   end
 end
