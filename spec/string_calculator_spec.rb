4d14ee211214d698c5d7b1ca51412ad7544f8b7d
Nirmal Yadav <nirmal.yadav00@gmail.com>
2024-05-01 00:36:40 +0530

    Add test for having '\n' between the input numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,7 +7,7 @@ describe StringCalculator do
 
   describe '#add' do
     shared_examples 'returns the sum of inputs' do |numbers, result|
-      it "returns #{result} for input #{numbers}" do
+      it "returns #{result} for input #{numbers.inspect}" do
         expect(calculator.add(numbers)).to eq(result)
       end
     end
@@ -16,5 +16,6 @@ describe StringCalculator do
     include_examples 'returns the sum of inputs', '1', 1
     include_examples 'returns the sum of inputs', '1,2', 3
     include_examples 'returns the sum of inputs', '1,2,3', 6
+    include_examples 'returns the sum of inputs', "1\n2,3", 6
   end
 end
