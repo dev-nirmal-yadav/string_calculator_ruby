c9972f386a78782e420aabec66606ce6fb6a3743
Nirmal Yadav <nirmal.yadav00@gmail.com>
2024-05-01 00:39:06 +0530

    Changes to handle "\n" between numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,5 @@
 class StringCalculator
   def add(numbers)
-    numbers.split(',').inject(0) { |sum, num| sum + num.to_i }
+    numbers.split(/[,\n]/).inject(0) { |sum, num| sum + num.to_i }
   end
 end
