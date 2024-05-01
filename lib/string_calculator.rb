c231d61a879f855d6edeb54f41fc83d658f16f23
Nirmal Yadav <nirmal.yadav00@gmail.com>
2024-05-01 14:12:44 +0530

    Changes to raise error for negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,6 @@
 class StringCalculator
   class InvalidInputError < StandardError; end
+  class NegativeNumberError < StandardError; end
 
   DEFAULT_DELIMITER = Regexp.new('[,\n]')
 
@@ -7,6 +8,9 @@ class StringCalculator
     validate_input(numbers)
 
     delimiter, numbers = extract_delimiter_and_numbers(numbers)
+
+    numbers_array = numbers.split(delimiter)
+    validate_negative_numbers(numbers_array)
 
     numbers.split(/#{delimiter}|\n/).inject(0) { |sum, num| sum + num.to_i }
   end
@@ -23,5 +27,11 @@ class StringCalculator
     numbers = numbers.sub(/^\/\/.+?\n/, '')
 
     [delimiter, numbers]
+  end
+
+  def validate_negative_numbers(numbers_array)
+    negative_numbers = numbers_array.select { |number| number.to_i.negative? }
+
+    raise NegativeNumberError, "Negative numbers not allowed #{negative_numbers.join(',')}" if negative_numbers.any?
   end
 end
