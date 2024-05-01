07997c8a4cd0854750e2b01d11cdfbebd38cbfcb
Nirmal Yadav <nirmal.yadav00@gmail.com>
2024-05-01 12:44:34 +0530

    Add changes to support custom delimeter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,7 +4,13 @@ class StringCalculator
   def add(numbers)
     validate_input(numbers)
 
-    numbers.split(/[,\n]/).inject(0) { |sum, num| sum + num.to_i }
+    delimiter = ','
+    if numbers.start_with?("//")
+      delimiter = numbers[2]
+      numbers = numbers[4..-1]
+    end
+
+    numbers.split(/#{delimiter}|\n/).inject(0) { |sum, num| sum + num.to_i }
   end
 
   private
