c4fe772d888f70fb455aba6f0897a08d6de2cef2
Nirmal Yadav <nirmal.yadav00@gmail.com>
2024-05-01 12:06:31 +0530

    Changes to raise error if input is invalid

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,15 @@
 class StringCalculator
+  class InvalidInputError < StandardError; end
+
   def add(numbers)
+    validate_input(numbers)
+
     numbers.split(/[,\n]/).inject(0) { |sum, num| sum + num.to_i }
+  end
+
+  private
+
+  def validate_input(numbers)
+    raise InvalidInputError, "Invalid input format: '#{numbers}'" if numbers.end_with?("\n")
   end
 end
