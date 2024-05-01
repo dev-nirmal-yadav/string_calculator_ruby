c3e4a71c04391d62affe79419a8826107419fb75
Nirmal Yadav <nirmal.yadav00@gmail.com>
2024-05-01 13:42:57 +0530

    Refactor changes to fetch delimiter and numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,14 +1,12 @@
 class StringCalculator
   class InvalidInputError < StandardError; end
 
+  DEFAULT_DELIMITER = Regexp.new('[,\n]')
+
   def add(numbers)
     validate_input(numbers)
 
-    delimiter = ','
-    if numbers.start_with?("//")
-      delimiter = numbers[2]
-      numbers = numbers[4..-1]
-    end
+    delimiter, numbers = extract_delimiter_and_numbers(numbers)
 
     numbers.split(/#{delimiter}|\n/).inject(0) { |sum, num| sum + num.to_i }
   end
@@ -17,5 +15,13 @@ class StringCalculator
 
   def validate_input(numbers)
     raise InvalidInputError, "Invalid input format: '#{numbers}'" if numbers.end_with?("\n")
+  end
+
+  def extract_delimiter_and_numbers(numbers)
+    match = numbers.match(/^\/\/(.+)\n/)
+    delimiter = match ? match[1] : DEFAULT_DELIMITER
+    numbers = numbers.sub(/^\/\/.+?\n/, '')
+
+    [delimiter, numbers]
   end
 end
