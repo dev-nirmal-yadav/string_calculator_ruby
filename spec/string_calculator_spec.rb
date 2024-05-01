92037cc58a9af2752fb39ed25b6cddf6e28d5203
Nirmal Yadav <nirmal.yadav00@gmail.com>
2024-05-01 11:33:54 +0530

    Add test for invalid input '1,\n'

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -12,10 +12,20 @@ describe StringCalculator do
       end
     end
 
-    include_examples 'returns the sum of inputs', '', 0
-    include_examples 'returns the sum of inputs', '1', 1
-    include_examples 'returns the sum of inputs', '1,2', 3
-    include_examples 'returns the sum of inputs', '1,2,3', 6
-    include_examples 'returns the sum of inputs', "1\n2,3", 6
+    context 'when inputs are valid' do
+      include_examples 'returns the sum of inputs', '', 0
+      include_examples 'returns the sum of inputs', '1', 1
+      include_examples 'returns the sum of inputs', '1,2', 3
+      include_examples 'returns the sum of inputs', '1,2,3', 6
+      include_examples 'returns the sum of inputs', "1\n2,3", 6
+    end
+
+    context 'when inputs are invalid' do
+      it "raises error for input '1,\n'" do
+        expect {
+          calculator.add("1,\n")
+        }.to raise_error(described_class::InvalidInputError, "Invalid input format: '1,\n'")
+      end
+    end
   end
 end
